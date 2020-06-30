b7c617a47fecdae0f563d4f712d8bc3d411613fe
hammlee <hm@daground.io>
2020-06-30 22:03:40 +0900

    JS: Home contact

--- main.js
+++ main.js
@@ -10,3 +10,31 @@ document.addEventListener('scroll', () => {
     navbar.classList.remove('navbar--dark');
   }
 })
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link
+  if (!link) {
+    return;
+  }
+  scrollIntoViews(link);
+
+  let matches = document.getElementsByClassName('navbar__menu__item');
+  for (let i = 0; i < matches.length; i++) {
+    matches[i].classList.remove('active');
+  }
+  target.classList.add('active');
+})
+
+// Handle scrolling when tapping on the "CONTACT ME" button
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', (event) => {
+  scrollIntoViews(event.target.dataset.link);
+})
+
+function scrollIntoViews(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
+}
