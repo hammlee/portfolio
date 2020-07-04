bc32ac05eb859b7751f600fa858a96f4fba98762
hammlee <hm@daground.io>
2020-07-04 23:35:43 +0900

    JS: Home Transparent

--- main.js
+++ main.js
@@ -21,6 +21,7 @@ navbarMenu.addEventListener('click', (event) => {
   }
   scrollIntoViews(link);
 
+  // Make navbar item active
   let matches = document.getElementsByClassName('navbar__menu__item');
   for (let i = 0; i < matches.length; i++) {
     matches[i].classList.remove('active');
@@ -38,3 +39,11 @@ function scrollIntoViews(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: 'smooth' });
 }
+
+// Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector('.home__container')
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  // console.log()
+  home.style.opacity = 1 - window.scrollY / homeHeight
+})
